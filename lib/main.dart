825d46c0ad0602283e2d93f2c50f642364812c7f
ImadDz-7 <imadbouamrirene97@gmail.com>
2025-05-15 14:34:06 +0000

    Duplicate the same design for the other Categories ( Family members, Colors, Phrases).

--- lib/main.dart
+++ lib/main.dart
@@ -23,10 +23,52 @@ class TokuApp extends StatelessWidget {
               alignment: Alignment.centerLeft,
               padding: const EdgeInsets.only(left: 16),
               width: double.infinity,
-              height: 65,
+              height: 70,
               color: Colors.orange,
               child: const Text(
                 'Numbers',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                ),
+              ),
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              padding: const EdgeInsets.only(left: 16),
+              width: double.infinity,
+              height: 70,
+              color: Colors.green,
+              child: const Text(
+                'Family Members',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                ),
+              ),
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              padding: const EdgeInsets.only(left: 16),
+              width: double.infinity,
+              height: 70,
+              color: Colors.purple,
+              child: const Text(
+                'Colors',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                ),
+              ),
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              padding: const EdgeInsets.only(left: 16),
+              width: double.infinity,
+              height: 70,
+              color: Colors.blue,
+              child: const Text(
+                'Phrases',
                 style: TextStyle(
                   color: Colors.white,
                   fontSize: 18,
