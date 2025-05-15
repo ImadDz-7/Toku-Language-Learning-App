c33a1442a6244ed0dfcde529f961500e2da55d5e
ImadDz-7 <imadbouamrirene97@gmail.com>
2025-05-15 13:14:52 +0000

    Create Toku App Project

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TokuApp());
+}
+
+class TokuApp extends StatelessWidget {
+  const TokuApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(),
+    );
+  }
+}
